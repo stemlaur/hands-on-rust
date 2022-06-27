2d042dfdbbbe65644b56531465a3acc47746561a
lstemmer <laurent-externe.stemmer@dalkia.fr>
2022-06-27 13:59:08 +0200

    followup on treehouse

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,20 +1,60 @@
 use std::io::stdin;
 
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
 
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
-    stdin()
-        .read_line(&mut your_name)
-        .expect("Failuuuuure");
-    your_name
-        .trim()
-        .to_lowercase()
+    stdin().read_line(&mut your_name).expect("Failuuuuure");
+    your_name.trim().to_lowercase()
 }
 
 fn main() {
-    println!("Hello, what's your name ?");
+    let mut visitor_list = vec![
+        Visitor::new("bert", "Hello bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi steve. Your milk in on the fridge."),
+        Visitor::new("fred", "Wow, who invited fred ?"),
+    ];
 
-    let your_name = what_is_your_name();
+    loop {
+        println!("Hello, what's your name ?");
 
-    println!("{:?}", your_name);
+        let your_name = what_is_your_name();
+
+        let known_visitor = visitor_list
+            .iter()
+            .find(|visitor| visitor.name == your_name);
+
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if your_name.is_empty() {
+                    break;
+                } else {
+                    println!("Sorry you are not on the list.");
+                    visitor_list.push(Visitor::new(&your_name, "New friend"));
+                }
+            }
+        };
+        break;
+    }
+
+    println!("The final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
